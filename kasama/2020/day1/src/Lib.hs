10a1aa21a024a04cb25abefac9750c23064408d8
Kasama <robertoaall@gmail.com>
2020-12-04 20:26:31 -0300

    Make day1 more succint

--- kasama/2020/day1/src/Lib.hs
+++ kasama/2020/day1/src/Lib.hs
@@ -3,28 +3,18 @@ module Lib
     ) where
 
 import Expenses (expenses)
-import Data.Maybe
 
 someFunc :: IO ()
 someFunc = do
   putStrLn "First exercise:"
-  print targeted
+  print $ head targeted
   putStrLn "Second exercise:"
-  print trioTargeted
+  print $ head trioTargeted
 
 target = 2020
 
-
 -- Exercise one
-sumPair (a, b) = if a + b == target then Just (a * b) else Nothing
-
-pairs = [(a, b) | a <- expenses, b <- expenses]
-values = map sumPair pairs
-targeted = filter isJust values
+targeted = [ a * b | a <- expenses, b <- expenses, a + b == target ]
 
 -- Exercise two
-sumTrio (a, b, c) = if a + b + c == target then Just (a * b * c) else Nothing
-
-trios = [(a, b, c) | a <- expenses, b <- expenses, c <- expenses]
-trioValues = map sumTrio trios
-trioTargeted = filter isJust trioValues
+trioTargeted = [a * b * c | a <- expenses, b <- expenses, c <- expenses, a + b + c == target]
