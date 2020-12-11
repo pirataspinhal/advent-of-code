6b92dc800dcc2cd52e8aab2f08abf89561038403
Kasama <robertoaall@gmail.com>
2020-12-11 15:54:00 -0300

    Add day 10

--- kasama/2020/app/day10/Main.hs
+++ kasama/2020/app/day10/Main.hs
@@ -1,50 +1,34 @@
 module Main where
 
-import Data.Sort ( sort )
-import Graph
+-- import Lib.Graph
 import Data.Graph
+import Data.List
+import qualified Data.Vector as Vector
 
+-- Graph helers
+data CGraph node a = CGraph { graph :: Graph
+                            , getNode :: Vertex -> (node, a, [a])
+                            , getVertex :: a -> Maybe Vertex
+                            }
+instance Show (CGraph a b) where
+  show cgraph = show $ graph cgraph
+toCGraph (a, b, c) = CGraph { graph = a, getNode = b, getVertex = c}
+
+-- Main
 main :: IO ()
 main = do
   input <- input
   putStr "number of 1-jolts multiplied by the number of 3-jolts: "
   print $ multOneThree $ findJoltDifferences input
+  putStr "number of possible transformer combinations: "
+  print $ possibleArrangements testInput
 
 input :: IO [Int]
 input = fmap read . lines <$> readFile "inputs/day10.in"
 
-testInput = [ 28
-            , 33
-            , 18
-            , 42
-            , 31
-            , 14
-            , 46
-            , 20
-            , 48
-            , 47
-            , 24
-            , 23
-            , 49
-            , 45
-            , 19
-            , 38
-            , 39
-            , 11
-            , 1
-            , 32
-            , 25
-            , 35
-            , 8
-            , 17
-            , 7
-            , 9
-            , 4
-            , 2
-            , 34
-            , 10
-            , 3
-            ]
+testInput :: [Int]
+testInput = [ 28 , 33 , 18 , 42 , 31 , 14 , 46 , 20 , 48 , 47 , 24 , 23 , 49 , 45 , 19
+            , 38 , 39 , 11 , 1 , 32 , 25 , 35 , 8 , 17 , 7 , 9 , 4 , 2 , 34 , 10 , 3 ]
 
 multOneThree (a, _, b) = a * b
 
@@ -56,6 +40,7 @@ findJoltDifferences transformers = calcJolts initialJolts sorted
       | value == 1 = (1, 0, 0)
       | value == 2 = (0, 1, 0)
       | value == 3 = (0, 0, 1)
+      | otherwise  = (0, 0, 0)
       where
         value = head sorted
     sorted = sort transformers
@@ -65,25 +50,25 @@ findJoltDifferences transformers = calcJolts initialJolts sorted
       | difference == 1 = calcJolts (ones + 1, twos, threes) next
       | difference == 2 = calcJolts (ones, twos + 1, threes) next
       | difference == 3 = calcJolts (ones, twos, threes + 1) next
-      | otherwise = calcJolts (ones, twos, threes) next
+      | otherwise       = calcJolts (ones, twos, threes) next
       where
         next = b:others
         difference = abs $ a - b
 
-makeGraph connections = toCGraph $ graphFromEdges edges
+-- >>> possibleArrangements (testInput)
+-- 19208
+possibleArrangements :: [Int] -> Int
+possibleArrangements values = Vector.head pathsAccumulator
   where
-    edges = map (\(a, b) -> (a, a, [b])) connections
-
--- >>> connectedTransformers [1,3,8,2,4,7]
--- [(1,3),(1,2),(1,4),(3,4),(2,3),(2,4),(4,7),(7,8)]
-connectedTransformers :: [Int] -> [(Int, Int)]
-connectedTransformers transformers = transformerCombinations
-  where
-    transformerCombinations = [ (snd a, snd b)
-                              | a <- indexedTransformers
-                              , b <- indexedTransformers
-                              , fst a /= fst b
-                              , abs (snd a - snd b) <= 3
-                              , snd a < snd b
-                              ]
-    indexedTransformers = zip [1..] transformers
+    allTransformerValues = 0:values
+    amountOfTransformers = length allTransformerValues
+    valuesVector = Vector.fromList (sort allTransformerValues)
+    pathsAccumulator = Vector.generate amountOfTransformers countArrangements
+    countArrangements index
+      | index == amountOfTransformers - 1 = 1
+      | otherwise = sum ((pathsAccumulator Vector.!) <$> indices)
+      where
+        indices = [ (index + 1)..(index + length validNexts) ]
+        validNexts = Vector.takeWhile isValidNextTransformer currentValues
+        currentValues = Vector.drop (index + 1) valuesVector
+        isValidNextTransformer val = abs (val - (valuesVector Vector.! index)) <= 3
